0b65fb06327a56545097e153d62bafc010c3c9ff
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-08-04 13:11:30 +0600

    Add Celebration.jsx component

--- src/components/Celebration.jsx
+++ src/components/Celebration.jsx
@@ -1,11 +1,9 @@
-// components/Celebration.jsx
 import React from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
 
 export default function Celebration({ show}) {
 
     if (!show) return null;
-
     return (
         <div className="fixed top-0 left-0 w-full h-full pointer-events-none z-[9999]">
                 <div className="flex items-center justify-center w-full h-full">
@@ -13,7 +11,7 @@ export default function Celebration({ show}) {
                         autoplay
                         controls={false}
                         loop={false}
-                        src="/celevration.json" // fireworks
+                        src="/celevration.json"
                         style={{ height: '140vh', width: "100%" }}
                     />
                 </div>
