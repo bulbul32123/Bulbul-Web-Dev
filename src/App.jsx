ab7172396b956b75925570d1deb418f24e2a3470
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-08-24 15:34:35 +0600

    App folder updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Toaster } from 'react-hot-toast';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import useLenis from './hooks/useLenis';
 import Home from './pages/Home';
-import About from './pages/About';
 import Projects from './pages/Projects';
 import Curve from './components/Curve';
 import SidebarLayout from './components/SidebarLayout';
@@ -47,7 +46,6 @@ function AnimatedRoutes() {
         <SidebarLayout>
           <Routes location={displayLocation}>
             <Route path="/" element={<Home />} />
-            <Route path="/aboutMe" element={<About />} />
             <Route path="/project" element={<Projects />} />
           </Routes>
         </SidebarLayout>
