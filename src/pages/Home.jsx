248a22f55edb010226e59672d5d4cc4ea84324d3
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-08-25 23:53:14 +0600

    updated on page folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 import About from "../sections/About";
 import Hero from "../sections/Hero";
-import Projects from '../sections/Projects';
-import Game from '../sections/Game';
+import TechStacks from "../sections/TechStacks";
+import Projects from "../sections/Projects";
+import Game from "../sections/Game";
 
 export default function Home() {
-
   return (
     <div className="">
-
       <div className="h-full w-full">
         <Hero />
         <div className="w-full h-full">
           <About />
+          <TechStacks />
         </div>
         <div className="h-full w-full">
           <Projects />
         </div>
         <div className="h-full w-full relative">
-        <Game />
+          <Game />
         </div>
       </div>
     </div>
