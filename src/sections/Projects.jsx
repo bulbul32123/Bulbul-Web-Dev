d1a8fc2d67ccfd61db421989842dff48bdfbce55
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-11-11 17:48:38 +0600

    Update Projects.jsx

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -4,6 +4,7 @@ import Card from './Card';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
+
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Projects() {
