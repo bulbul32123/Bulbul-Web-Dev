bb2cff0dccda4cbe444847bcf05372971e423bac
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-11-12 17:48:56 +0600

    Update TechStacks.jsx

--- src/sections/TechStacks.jsx
+++ src/sections/TechStacks.jsx
@@ -1,5 +1,6 @@
 import React, { useMemo, useState } from "react";
 import { motion } from "framer-motion";
+
 import { techStacks } from "../utils/TechStacks";
 import TechStacksTitle from "./TechStacksTitle";
 
