bae894f01816fe63f7b2413edc06f93cf8f5b026
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-11-14 17:50:02 +0600

    Update ClientPic.jsx

--- src/components/ClientPic.jsx
+++ src/components/ClientPic.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
 export default function ClientPic({ text, bg }) {
-    console.log(text, bg);
-
     return (
         <div className={`py-3 px-5 rounded-full`} style={{ backgroundColor: bg }}>
             {text}
