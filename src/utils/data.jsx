50c016cb12a774f888d242b564e19cbcf995fd5d
Bulbul Islam <mdbulbulislamtheprogrammer@gmail.com>
2026-07-24 01:21:19 +0600

    updated

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -1,22 +1,79 @@
-import React from 'react';
+import ExpressIcon from "../../public/icons/techStacks/Express";
+import FigmaIcon from "../../public/icons/techStacks/Figma";
+import MongoDbIcon from "../../public/icons/techStacks/MongoDb";
 import NextIcon from "../../public/icons/techStacks/NextIcon";
+import NodeIcon from "../../public/icons/techStacks/Node";
 import ReactIcon from "../../public/icons/techStacks/React";
 import ReduxIcon from "../../public/icons/techStacks/Redux";
 import SassIcon from "../../public/icons/techStacks/Sass";
 import TailwindIcon from "../../public/icons/techStacks/Tailwind";
-import StrapiIcon from "../../public/icons/techStacks/Strapi";
-import StripeIcon from "../../public/icons/techStacks/Stripe";
-import FigmaIcon from "../../public/icons/techStacks/Figma";
-import FramerMotionIcon from "../../public/icons/techStacks/FramerMotion";
 
 export const projectData = [
   {
     id: 0,
-    title: 'Movix',
-    slug: "movix",
-    desc: 'Movix is a feature-rich movie discovery platform where users can explore millions of movies, TV shows, and celebrities. Built with advanced search and filtering capabilities, it offers trending, upcoming, and top-rated content — all in a sleek, modern UI. Powered by APIs, optimized for performance.',
-    link: 'https://advancemovieapp.netlify.app/',
-    imgSrc: '/images/project-1.webp',
+    title: "POS Inventory Management ",
+    link: "https://inventra-me.netlify.app/",
+    imgSrc: "/images/inventra.png",
+    stacks: [
+      {
+        name: "Nextjs",
+        icon: NextIcon,
+      },
+      {
+        name: "Tailwindcss",
+        icon: TailwindIcon,
+      },
+      {
+        name: "MongoDb",
+        icon: MongoDbIcon,
+      },
+      {
+        name: "Node.js",
+        icon: NodeIcon,
+      },
+      {
+        name: "Express.js",
+        icon: ExpressIcon,
+      },
+    ],
+    status: ["All", "/", " Design", "/", " Function"],
+    gitLink: "https://github.com/bulbul32123/Inventra",
+  },
+  {
+    id: 0,
+    title: "Payra",
+    link: "https://payra-flax.vercel.app/",
+    imgSrc: "/images/payra.png",
+    stacks: [
+      {
+        name: "Nextjs",
+        icon: NextIcon,
+      },
+      {
+        name: "Tailwindcss",
+        icon: TailwindIcon,
+      },
+      {
+        name: "MongoDb",
+        icon: MongoDbIcon,
+      },
+      {
+        name: "Node.js",
+        icon: NodeIcon,
+      },
+      {
+        name: "Express.js",
+        icon: ExpressIcon,
+      },
+    ],
+    status: ["All", "/", " Design", "/", " Function"],
+    gitLink: "https://github.com/bulbul32123/Payra",
+  },
+  {
+    id: 0,
+    title: "Movix",
+    link: "https://advancemovieapp.netlify.app/",
+    imgSrc: "/images/project-1.webp",
     stacks: [
       {
         name: "Reactjs",
@@ -31,48 +88,15 @@ export const projectData = [
         icon: ReduxIcon,
       },
     ],
-    status: ['All', '/' ," Design", "/", " Function"],
-    gitLink: 'https://github.com/bulbul32123/Movie-APP-With-React'
+    status: ["All", "/", " Design", "/", " Function"],
+    gitLink: "https://github.com/bulbul32123/Movie-APP-With-React",
   },
-  {
-    id: 75675,
-    title: "Online Ekart Store",
-    slug: "online-ekart-store",
-    desc: 'A fully responsive modern e-commerce platform offering a curated selection of tech products for work, travel, and daily use. Features product filtering, shopping cart, dynamic routing, and a seamless shopping experience. Built with React, Redux, and TailwindCSS.',
-    link: 'https://ekart-frontend-mu.vercel.app/',
-    imgSrc: '/images/project-2.webp',
-    stacks: [
-      {
-        name: "Nextjs",
-        icon: NextIcon,
-      },
-      {
-        name: "Tailwidcss",
-        icon: TailwindIcon,
-      },
-      {
-        name: "Stripe",
-        icon: StripeIcon,
-      },
-      {
-        name: "Strapi",
-        icon: StrapiIcon,
-      },
-      {
-        name: "Redux",
-        icon: ReduxIcon,
-      },
-    ],
-    status: ['All', ' /' ," Design", " /", " Function", " /", " Payment Gatway"],
-    gitLink: 'https://github.com/bulbul32123/Ekart-Store-Frontend'
-  },
+
   {
     id: 99,
     title: "Youtube",
-    slug: 'youtube',
-    desc: 'A full-featured YouTube clone built using React and TailwindCSS. It supports real-time search, video playback, channel view, and suggested content — mimicking YouTube’s core functionality. This project focuses on UI accuracy, responsiveness, and user interaction.',
-    link: 'https://youtubeclonewithreactjs.netlify.app/',
-    imgSrc: '/images/project-3.webp',
+    link: "https://youtubeclonewithreactjs.netlify.app/",
+    imgSrc: "/images/project-3.webp",
     stacks: [
       {
         name: "Reactjs",
@@ -87,14 +111,12 @@ export const projectData = [
         icon: FigmaIcon,
       },
     ],
-    status: ['All', ' /' ," Clone", " /", " Function"],
-    gitLink: 'https://github.com/bulbul32123/Youtube-Clone'
+    status: ["All", " /", " Clone", " /", " Function"],
+    gitLink: "https://github.com/bulbul32123/Youtube-Clone",
   },
   {
     id: 234,
-    title: 'Google Searcher',
-    slug: "google-searcher",
-    desc: 'A minimal Google clone that lets users perform real-time web searches and returns results just like the original. Uses Google Search API and focuses on speed, simplicity, and clean UI/UX. Designed to replicate the Google homepage experience.',
+    title: "Google Searcher",
     stacks: [
       {
         name: "Reactjs",
@@ -109,69 +131,9 @@ export const projectData = [
         icon: FigmaIcon,
       },
     ],
-    link: 'https://google-searcher.netlify.app/',
-    imgSrc: '/images/project-4.webp',
-    gitLink: 'https://github.com/bulbul32123/Google-Search-App',
-    status: ['All', ' /' ," Clone", " /", " Function"]
+    link: "https://google-searcher.netlify.app/",
+    imgSrc: "/images/project-4.webp",
+    gitLink: "https://github.com/bulbul32123/Google-Search-App",
+    status: ["All", " /", " Clone", " /", " Function"],
   },
-  {
-    id: 2342344,
-    title: 'E-Bike Store',
-    slug: "e-bike-store",
-    desc: 'An e-commerce storefront for electric bikes with a futuristic, clean UI. Features include responsive layouts, product showcases, smooth animations, and a user-focused design. Built for performance and modern shopping experiences.',
-    stacks: [
-      {
-        name: "Reactjs",
-        icon: ReactIcon,
-      },
-      {
-        name: "Tailwidcss",
-        icon: TailwindIcon,
-      },
-      {
-        name: "Redux",
-        icon: ReduxIcon,
-      },
-      {
-        name: "Figma",
-        icon: FigmaIcon,
-      },
-      {
-        name: "Stripe",
-        icon: StripeIcon,
-      },
-    ],
-    link: 'https://e-bike-store.netlify.app/',
-    imgSrc: '/images/project-5.webp',
-    gitLink: 'https://github.com/bulbul32123/E---Bike-Store',
-    status: ['All', ' /' ," Design", " /" ," Function"]
-  },
-  {
-    id: 144545564,
-    title: 'Dev Easy Browser',
-    slug: "dev-easy-browser",
-    desc: 'Dev Easy is a standalone browser + devtool app designed to speed up development for modern web developers. It includes utilities for testing responsiveness, accessibility, and performance. Built to enhance productivity and eliminate workflow bottlenecks.',
-    link: 'https://deveasy.netlify.app/',
-    imgSrc: '/images/project-6.webp',
-    stacks: [
-      {
-        name: "Nextjs",
-        icon: NextIcon,
-      },
-      {
-        name: "Tailwidcss",
-        icon: TailwindIcon,
-      },
-      {
-        name: "Framer Motion",
-        icon: FramerMotionIcon,
-      },
-      {
-        name: "Figma",
-        icon: FigmaIcon,
-      },
-    ],
-    status: ['All', ' /' ," Design"],
-    gitLink: 'https://github.com/bulbul32123/Dev-Easy---with-Reactjs'
-  }
 ];
